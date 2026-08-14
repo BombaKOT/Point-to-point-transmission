823073de2c6e3237fd99ba263e6c915a73621875
Bomba <d.ashitkin@gmail.com>
2026-08-14 15:25:54 -0400

    Add files via upload

---
+++ ProgramSERVER.cs
@@ -0,0 +1,220 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+await ConsoleManager.Main();
+public static class ConsoleManager {
+    private static bool running = true;
+    private static string request;
+    private static string[] rules = ["","","","","","","","","",];
+    private static readonly string[] configStrings = ["Open UDP Port (self)", 
+    "Open TCP Port (self)", 
+    "Bandwidth (self)", 
+    "Open UDP Port (server)", 
+    "Open TCP Port (server)", 
+    "Server IP Address", 
+    "Server bandwidth", 
+    "Greeting (to client) [NA for non-applicable]",
+    "Greeting (to server) [NA for non-applicable]"];
+    private static string repository;
+    public static async Task Main()
+    {
+        while(running)
+        {
+            for(int i = 0; i < rules.Length; i++)
+                Console.Write($"\n{configStrings[i]}: {rules[i]}");
+            request = ReadUserRes(1, ["Query"])[0];
+            await Decission(request);
+        }
+    }
+
+    private static string[] ReadUserRes(int times, string[] messages)
+    {
+        string[] res = new string[times];
+        for(int i = 0; i < times; i++)
+        {
+            Console.Write($"\n{messages[i]}: ");
+            res[i] = Console.ReadLine().Trim();
+        }
+        return res;
+    }
+
+    private static async Task Decission(string query)
+    {
+        switch (query.ToLower())
+        {
+            case "config": 
+                rules = ReadUserRes(9, configStrings);
+                Config config = new Config();
+                config.SetUp(rules);
+                break;
+            case "connects":
+                await ServerManager.Connect(false); 
+                break;
+            case "connectc": 
+                await ServerManager.Connect(true); 
+                break;
+            case "send": 
+                await ServerManager.SendTo(ReadUserRes(1, ["Filename"])[0]);
+                break;
+            case "disconnect":
+                await ServerManager.Disconnect();
+                break;
+            case "recieve":
+                await ServerManager.RecieveFrom();
+                break;
+            case "close":
+                Console.Write("\nClosing app");
+                running = false;
+                break;
+            case "recreate":
+                await ServerManager.Recreate(ReadUserRes(1, ["Name for recreation [NA for non-applicable]"])[0]);
+                break;
+            case "standby":
+                /*
+                    TODO:
+                        RENAME previous server names to listening device 
+                        Config lite - No dest EPs, ask if completely public or restricted to a couple of IP addresses, repo directory
+                        Create functions: RequestServerFile, UploadServerFile, StandbyConnect, StandbyDisconnect/Stop
+                */
+                repository = ReadUserRes(1, ["Repository of your files"])[0];
+                break;
+        }
+    }
+
+}
+
+
+
+public static class ServerManager
+{
+    private static Socket tcpSocket;
+    private static Socket udpSocket;
+
+    public static IPEndPoint[] EndPoints(int clientUDPPort, int clientTCPPort, string ip = null)
+    {
+        //Returns  EPs for future usage 
+        IPEndPoint serverUDP = new IPEndPoint(IPAddress.Parse(ip != null ? ip : SelfIpAddress()), clientUDPPort);
+        IPEndPoint serverTCP = new IPEndPoint(IPAddress.Parse(ip != null ? ip : SelfIpAddress()), clientTCPPort);
+        IPEndPoint[] vals = new IPEndPoint[2];
+        vals[0] = serverUDP;
+        vals[1] = serverTCP;
+        return vals;
+    }
+
+    private static void Initialize(int udpPort, int tcpPort)
+    {
+        //Creates the sockets and binds them to client EPs
+        IPEndPoint[] clientEPS = EndPoints(udpPort, tcpPort);
+
+        tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        IPEndPoint clientUDPEP = clientEPS[0];
+        IPEndPoint clientTCPEP = clientEPS[1];
+        tcpSocket.Bind(clientTCPEP);
+        udpSocket.Bind(clientUDPEP);
+    }
+
+    private static string SelfIpAddress()
+    {
+        string hostName = Dns.GetHostName();
+        return Dns.GetHostByName(hostName).AddressList[0].ToString();
+    }
+
+    public static void SetUp(Config config)
+    {
+        Initialize(config.clientUDPPort, config.clientTCPPort);
+        DriveTransmition.SetSockets(tcpSocket, udpSocket);
+        
+        IPEndPoint[] serverEPS = EndPoints(config.serverUDPPort, config.serverTCPPort, config.serverIP);
+        DriveTransmition.SetEP(serverEPS[0], serverEPS[1]);
+        
+        DriveTransmition.SetInfo(config.key2Server, config.key2Client, config.bandwidthClient, config.bandwidthServer);
+    } 
+
+    public static async Task SendTo(string filePath)
+    {
+        if (Path.Exists(filePath))
+        {
+            Console.Write($"\nSending file {filePath}");
+            await DriveTransmition.SendFile(filePath);
+            Console.Write($"\nFinished sending file {filePath}");
+        } else
+        {
+            Console.Write($"\nFilepath doesn't exist {filePath}");
+        }
+    }
+
+    public static async Task RecieveFrom()
+    {
+        Console.Write("\nAwaiting file");
+        await DriveTransmition.RecieveFile();
+        Console.Write("\nFinished receiving and constructing file, command 'recreate' to rebuild a file");
+    }
+
+    public static async Task Connect(bool client)
+    {
+        if (client)
+        {
+            Console.Write("\nConnecting you to dest server");
+            await DriveTransmition.ClientConnect();
+            Console.Write("\nFinished connecting, ready to send and recieve files");
+        } else
+        {
+            Console.Write("\nAwaiting connection from client");
+            await DriveTransmition.ServerConnect();
+            Console.Write("\nConnected to client, ready to send or recieve files");
+        }
+    }
+
+    public static async Task Disconnect()
+    {
+        Console.Write("\nBeginning process of disconnection");
+        await DriveTransmition.ServerConnect();
+        Console.Write("\nConnected to client, ready to send or recieve files");
+    }
+
+    public static async Task Recreate(string name = null)
+    {
+        Console.Write($"\nRecreating last recieved file");
+        DriveTransmition.CreateFile(name);
+        Console.Write($"\nFinished recreating last recieved file");
+    }
+
+    public static async Task StandbyServer(string repo)
+    {
+        
+    }
+}
+
+
+public class Config
+{
+    public int clientUDPPort;
+    public int clientTCPPort;
+    public int bandwidthClient;
+    public int serverUDPPort;
+    public int serverTCPPort;
+    public string serverIP;
+    public int bandwidthServer;
+    public string key2Server;
+    public string key2Client;
+    public void SetUp(string[] rules)
+    {
+        Int32.TryParse(rules[0], out clientUDPPort);
+        Int32.TryParse(rules[1], out clientTCPPort);
+        Int32.TryParse(rules[2], out bandwidthClient);
+        Int32.TryParse(rules[3], out serverUDPPort);
+        Int32.TryParse(rules[4], out serverTCPPort);
+        serverIP = rules[5];
+        if(rules[6] != "NA")
+            Int32.TryParse(rules[6], out bandwidthServer);
+        else 
+            bandwidthServer = 0;
+        key2Client = rules[7] != "NA" ? rules[7] : null;
+        key2Server = rules[8] != "NA" ? rules[8] : null;
+    }
+}
---
+++ Transmitions.cs
@@ -0,0 +1,193 @@
+using System.Net.Sockets;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+public static class DriveTransmition
+{
+    private static Socket connectionTCPSocket;
+    private static Socket socketTCP;
+    private static Socket socketUDP;
+    private static int bandwidth;
+    private static int bandwidthServer;
+    private static IPEndPoint udpEP;
+    private static IPEndPoint tcpEP;
+    private static string serverGreeting;
+    private static string selfPasskey;
+    private static string binPath = "bin.bin";
+    private static string lastName;
+    private static string lastExtension;
+
+    //CONFIG
+    public static void SetSockets(Socket tcpSocketNew, Socket udpSocketNew)
+    {
+        socketTCP = tcpSocketNew;
+        socketUDP = udpSocketNew;
+    }
+
+    public static void SetEP(IPEndPoint desEPUDP, IPEndPoint desEPTCP)
+    {
+        udpEP = desEPUDP;
+        tcpEP = desEPTCP;
+    }
+
+    public static void SetInfo(string greeting, string key, int transBandwidth, int serverBandwidth = 0)
+    {
+        serverGreeting = greeting;
+        selfPasskey = key;
+        bandwidth = transBandwidth;
+        bandwidthServer = serverBandwidth;
+    }
+
+    private static async Task<string> RecieveMessageUDP(int width)
+    {
+        byte[] buffer = new byte[width];
+        Memory<byte> mem = new Memory<byte>(buffer);
+        
+        var res = await socketUDP.ReceiveMessageFromAsync(mem, udpEP);
+        int length = res.ReceivedBytes;
+        return Encoding.ASCII.GetString(buffer, 0, length);
+    }
+
+    private static async Task SendMessageUDP(string contents)
+    {
+        await socketUDP.SendToAsync(Encoding.ASCII.GetBytes(Path.GetFileName(contents)), udpEP);
+    }
+
+    //TRANSMITIONS
+    public async static Task SendFile(string filePath)
+    {
+        byte[] fileBytes = File.ReadAllBytes(filePath);
+        int length = fileBytes.Length;
+
+        await SendMessageUDP(Path.GetFileName(filePath));
+        await SendMessageUDP(length.ToString());
+
+        int leftOver = length % bandwidth;
+        int whole = (length - leftOver) / bandwidth;
+
+        byte[] tempArray = new byte[bandwidth];
+
+        for(int i = 0; i < whole; i++)
+        {
+            int o = bandwidth * i;
+
+            for(int k = 0; k < bandwidth; k++)
+                tempArray[k] = fileBytes[k + o];
+
+            await connectionTCPSocket.SendAsync(tempArray);
+        }
+
+        tempArray = new byte[leftOver];
+
+        for(int i = whole * 16384; i < leftOver; i++)
+            tempArray[i] = fileBytes[i];
+        
+        await connectionTCPSocket.SendAsync(tempArray);
+    }
+
+    public async static Task RecieveFile()
+    {
+        int length;
+        int currentLength = 0;
+        int totalLength;
+
+        byte[] buffer;
+        Memory<byte> mem;
+        
+        int width = bandwidthServer != 0 ? bandwidthServer : bandwidth;
+        
+        string fileName = await RecieveMessageUDP(width);
+        
+        string fileLength = await RecieveMessageUDP(width);
+        int.TryParse(fileLength, out totalLength);
+
+        if(!Path.Exists(binPath))
+            File.Create(binPath).Close();
+
+        using(var stream = new FileStream(binPath, FileMode.Append, FileAccess.Write, FileShare.None, 16384, useAsync: true))
+        {
+            while(currentLength < totalLength)
+            {
+                buffer = new byte[width];
+                mem = new Memory<byte>(buffer);
+
+                length = await socketTCP.ReceiveAsync(mem);
+                currentLength += length;
+                await stream.WriteAsync(buffer, 0, length);
+            }
+            await stream.FlushAsync();
+            stream.Close();
+        }
+        CreateFile(fileName);  
+    }
+
+    public async static Task Disconnect()
+    {
+        if(connectionTCPSocket != null)
+        {
+            connectionTCPSocket.Close();
+            connectionTCPSocket = null;
+            return;
+        } 
+        await socketTCP.DisconnectAsync(true);
+    }
+
+    public async static Task ClientConnect()
+    {
+        if(serverGreeting != null)
+        {
+            byte[] greeting = Encoding.ASCII.GetBytes(serverGreeting);
+            await socketUDP.SendToAsync(greeting, udpEP);
+        }
+
+        await socketTCP.ConnectAsync(tcpEP);
+    }
+
+    public async static Task ServerConnect()
+    {
+        if (selfPasskey != null)
+        {
+            bool passed = false;
+            int keyByteLength = Encoding.ASCII.GetByteCount(selfPasskey);
+            string attempt;
+
+            while (!passed)
+            {
+                attempt = await RecieveMessageUDP(keyByteLength);
+
+                if(attempt.Equals(selfPasskey)) break;
+            }
+        }
+
+        connectionTCPSocket = await socketTCP.AcceptAsync();
+    }
+
+    //OTHER
+    private static void AddLog(string info, string logPath)
+    {
+        File.AppendAllText(logPath, $"{DateTime.Today}\n{info}\n\n");
+    }
+
+    public static void CreateFile(string file = null)
+    {
+        if(file != null)
+        {
+            lastName = Path.GetFileName(file);
+            lastExtension = Path.GetExtension(file); 
+        }
+
+        string path;
+        int index = 1;
+
+        do {path = lastName + index++ + lastExtension;} while (Path.Exists(path));
+        
+        Console.Write($"Building {path}");
+
+        File.Copy(binPath, path);
+
+        Console.Write($"Finished building {path}");
+    }
+
+}
